fa73c8ba9417f1663fc78c1b352637505a96fb6b
quynhvp90 <quynh.nguyenhuu@imipgroup.com>
2024-12-13 16:52:24 +0700

    update js tiny

--- Dentistry.Admin/wwwroot/js/tinymce.js
+++ Dentistry.Admin/wwwroot/js/tinymce.js
@@ -5,6 +5,20 @@
     images_upload_url: '/upload-image',
     automatic_uploads: true,
     file_picker_types: 'image',
+    plugins: [
+        // Core editing features
+        'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+        // Your account includes a free trial of TinyMCE premium features
+        // Try the most popular premium features until Dec 27, 2024:
+        'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown', 'importword', 'exportword', 'exportpdf'
+    ],
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+    mergetags_list: [
+        { value: 'First.Name', title: 'First Name' },
+        { value: 'Email', title: 'Email' },
+    ],
     file_picker_callback: function (callback, value, meta) {
         var input = document.createElement('input');
         input.setAttribute('type', 'file');
@@ -13,11 +27,22 @@
         debugger;
         input.onchange = function () {
             var file = this.files[0];
-            var reader = new FileReader();
-            reader.onload = function () {
-                callback(reader.result, { alt: file.name });
-            };
-            reader.readAsDataURL(file);
+            var formData = new FormData();
+            formData.append('file', file);
+
+            // Send the file via fetch
+            fetch('/upload-image', {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Upload response:', data);  // Debug the response
+                    callback(data.path, { alt: file.name });  // Update TinyMCE with image URL
+                })
+                .catch(error => {
+                    console.error('Error uploading image:', error);
+                });
         };
 
         input.click();
