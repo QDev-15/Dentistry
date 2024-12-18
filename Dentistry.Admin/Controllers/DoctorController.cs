67e8fabfcc96fac3f17275c82d74c75a06e13d49
Nguyễn Hữu Quỳnh <quynhvpit@outlook.com>
2024-12-18 09:32:53 +0700

    update doctor controller

--- Dentistry.Admin/Controllers/DoctorController.cs
+++ Dentistry.Admin/Controllers/DoctorController.cs
@@ -1,10 +1,13 @@
-﻿using Dentisty.Data;
+﻿using Dentistry.ViewModels.Catalog.Doctors;
+using Dentistry.ViewModels.Common;
+using Dentisty.Data;
 using Dentisty.Data.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Dentistry.Admin.Controllers
 {
-    public class DoctorController : Controller
+    public class DoctorController : BaseController
     {
 
         private readonly IDoctorRepository _doctorRepository;
@@ -21,6 +24,34 @@ namespace Dentistry.Admin.Controllers
         public IActionResult List()
         {
             return ViewComponent("DoctorList");
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddEdit(int id) {
+            if (id == 0) {
+                return View(new DoctorVm());
+            } else
+            {
+                var doctor = await _doctorRepository.GetById(id);
+                return View(doctor.ReturnViewModel());
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEdit(DoctorVm item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid data");
+            }
+            if (item == null) return Json(new { });
+            if (item.Id == 0)
+            {
+                var doctor = await _doctorRepository.Create(item);
+            }
+            else
+            {
+                var doctor = await _doctorRepository.Update(item);
+            }
+            return Json(new SuccessResult<bool>());
         }
     }
 }
