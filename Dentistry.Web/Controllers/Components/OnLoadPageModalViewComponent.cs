7246fca848d92b2db8e3f2ba6369e92f68b9ecde
Nguyễn Hữu Quỳnh <quynhvpit@outlook.com>
2024-12-24 09:32:19 +0700

    add modal on default

---
+++ Dentistry.Web/Controllers/Components/OnLoadPageModalViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Dentisty.Data.Interfaces;
+using Dentistry.ViewModels.Catalog.AppSettings;
+using Microsoft.AspNetCore.Mvc;
+using Dentistry.ViewModels.Catalog.Contacts;
+
+namespace Dentistry.Web.Controllers.Components
+{
+    public class OnLoadPageModalViewComponent : ViewComponent
+    {
+        public OnLoadPageModalViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var result = new ContactVm();
+            return View("~/Views/Contact/Partials/_onLoadPageModal.cshtml", result);
+        }
+    }
+}
