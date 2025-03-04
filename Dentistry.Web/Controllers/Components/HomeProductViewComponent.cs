8e752b673e688ff57981ea14f37e785cf42691c5
quynhvp90 <quynh.nguyenhuu@imipgroup.com>
2025-03-04 15:24:04 +0700

    update setting app

--- Dentistry.Web/Controllers/Components/HomeProductViewComponent.cs
+++ Dentistry.Web/Controllers/Components/HomeProductViewComponent.cs
@@ -4,16 +4,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Dentistry.Web.Controllers.Components
 {
-    public class HomeNewsViewComponent : ViewComponent
+    public class HomeProductViewComponent : ViewComponent
     {
         private readonly IArticleRepository _articleRepository;
-        public HomeNewsViewComponent(IArticleRepository articleRepository) {
+        public HomeProductViewComponent(IArticleRepository articleRepository) {
             _articleRepository = articleRepository;
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var news = await _articleRepository.GetForApplication(ArticleType.News);
-            return View("~/Views/ViewComponents/HomeNews.cshtml", news.Take(6).ToList());
+            var news = await _articleRepository.GetForApplication(ArticleType.Products);
+            return View("~/Views/ViewComponents/HomeProduct.cshtml", news.Take(6).ToList());
         }
     }
     
