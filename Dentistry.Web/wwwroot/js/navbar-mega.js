13f61a4c96b161fe8327a9b48239c0163ba9ebdf
quynhvp90 <quynh.nguyenhuu@imipgroup.com>
2025-03-17 17:48:35 +0700

    update css for menu

--- Dentistry.Web/wwwroot/js/navbar-mega.js
+++ Dentistry.Web/wwwroot/js/navbar-mega.js
@@ -1,6 +1,88 @@
 ﻿$(document).ready(function () {
     // executes when HTML-Document is loaded and DOM is ready
     var queueAction = [];
+    var hoverAny = {
+        lv2: null,
+        lv3: null
+    };
+    var intevalResetPrevious = null;
+    $('[class*="li-lv2-hover-"]').hover(
+        function () {
+            // mouse hover
+            // resset interval neu co
+            if (intevalResetPrevious) {
+                clearInterval(intevalResetPrevious);
+                intevalResetPrevious = null;
+            }
+            
+            // hiển thị UL
+            let matchedClass = $(this).attr("class").match(/li-lv2-hover-(\d+)/);
+            if (matchedClass) {
+                var index = matchedClass[1]; // Lấy số thứ tự
+                if (index != hoverAny.lv2) {
+                    if (hoverAny.lv2) {
+                        // reset lại previous
+                        resetHover(hoverAny);
+                    }
+                    $(this).addClass("lv2-active");
+                    hoverAny.lv2 = index;
+                    hoverAny.lv3 = null;
+                    // Ẩn tất cả ul-lv3 ngay lập tức
+                    $('[class*="ul-lv3-"]').stop(true, true).fadeOut(200);
+
+                    // Hiển thị ul-lv3 tương ứng sau 0.2s
+                    setTimeout(() => {
+                        if (hoverAny.lv2) {
+                            $(".ul-lv3-" + hoverAny.lv2).stop(true, true).fadeIn(200);
+                            $(".i-caret-l-" + hoverAny.lv2).stop(true, true).fadeIn(200);
+                            $(".i-caret-r-" + hoverAny.lv2).stop(true, true).fadeOut(100);
+                            // hiển thị Menu lv3
+                            $(".menu-lv3").stop(true, true).fadeIn(200);
+                        }
+                    }, 200);
+                }
+            }
+        },
+        function () {
+            // đợi 0.2s nếu không có action thì reset
+            fadeOutAny();
+        }
+    );
+
+    // level 3 hover
+    $('[class*="li-lv3"]').hover(
+        function () {
+            // hover
+            hoverAny.lv3 = 'active';
+        },
+        function () {
+            // un-hover
+            hoverAny.lv3 = 'non-active';
+            fadeOutAny();
+        }
+    );
+    function fadeOutAny() {
+        if (intevalResetPrevious) {
+            clearInterval(intevalResetPrevious);
+            intevalResetPrevious = null;
+        }
+        intevalResetPrevious = setInterval(() => {
+            console.log("lv3 = ", hoverAny.lv3);
+            if (hoverAny.lv3 != 'active') {
+                resetHover(hoverAny);
+            }
+            clearInterval(intevalResetPrevious);
+            intevalResetPrevious = null;
+        }, 200);
+    }
+    function resetHover(hover) {
+        $('[class*="menu-lv3"]').stop(true, true).fadeOut(200);
+        if (hover.lv2) {
+            $('.li-lv2').removeClass('lv2-active');
+            $(".i-caret-l-" + hover.lv2).stop(true, true).fadeOut(100);
+            $(".i-caret-r-" + hover.lv2).stop(true, true).fadeIn(100);
+        }
+    }
     // breakpoint and up  
     $(window).resize(function () {
         initNav();
